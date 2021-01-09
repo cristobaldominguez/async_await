4aed7c67c1dc2c959b1d7b364ab6b3434e4dcdd6
Cristobal Dominguez <cristobald@gmail.com>
2021-01-09 09:01:30 -0300

    Adds Index File

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const fs = require('fs')
 const http = require('http')
 
 const server = http.createServer((req,res) => {
+    // index.html
+    if (req.url === '/' && req.method === 'GET') {
+        fs.readFile('./views/index.html', (err, file) => {
+            if (err) console.error(err)
 
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end(file)
+        })
+    }
 })
 
 server.listen(port, _ => console.log(`Servidor funcionando en puerto ${port}`))
